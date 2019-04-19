6522ba9a9148a715f09b7a4e013a42678d01400d
pammurray10 <pammcinnis10@gmail.com>
2019-04-19 08:24:00 -0400

    updated all files

--- thesaurus_api.js
+++ thesaurus_api.js
@@ -4,6 +4,7 @@ const apiKey = '9ac261cd-f87f-47c0-aa8c-7cde65b78ac2'
 
 let word;
 
+
 app.getSynonyms = function(word) {
     $.ajax({
         url: `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}`,
@@ -27,12 +28,19 @@ app.getSynonyms = function(word) {
     });
 }
 
+
 app.displayWords = function(ant, syn) {
     const antListHtml = ant.map(item => {
-        return `<li>${item}</li>`;
-    }); 
+        return `<li class="">${item}</li>`;
+    });
+    const synListHtml = syn.map(item => {
+        return `<li class="">${item}</li>`;
+    });    
     $('.antonym-list').html(antListHtml);
+    $('.synonym-list').html(synListHtml.slice(0, 5));
 }
+
+
 
 app.init = function() {
 
@@ -41,6 +49,14 @@ app.init = function() {
         const word = $('input[type=text]').val();
         app.getSynonyms(word);
     });
+
+    $('.x').on('click', function() {
+        $('.modal').addClass( "closeModal" );
+    });
+
+    $('.new-word-button').on('click', function() {
+        $('.modal').removeClass("closeModal").addClass("openModal");
+    })
 };
 
 
